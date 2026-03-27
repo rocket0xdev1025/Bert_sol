84dc6a000157111050e90c58d636a80e8a2bd60c
rocket0xdev1025 <rocket0xdev@gmail.com>
2026-03-28 08:37:09 +0900

    first commit

---
+++ framerusercontent.com/sites/3oemcGuaNVrSPm6h8kbNHG/chunk-Q5L4SLIB.mjs
@@ -0,0 +1,3 @@
+function a(t,e){return{description:`Community-powered with a mission to help dogs worldwide. Doing good, one paw at a time. \u{1F43E}
+`,favicon:"/framerusercontent.com/assets/VJnjSC1Mf23IHvcEBIN7VPuqAR0.png",robots:"max-image-preview:large",socialImage:"/framerusercontent.com/assets/plKxWEepvVwn8mg4O2vQhSAeXqI.png",title:"$BERT - Mayor of Solana."}}export{a};
+//# sourceMappingURL=chunk-Q5L4SLIB.mjs.map
